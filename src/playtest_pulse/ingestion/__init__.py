f89907ec32f4d790dee09dc7e627d1431033df89
mruke <mruke1991@outlook.com>
2026-04-26 14:22:00 -0300

    Removed leftover artifact from previous version of init for ingestion

--- src/playtest_pulse/ingestion/__init__.py
+++ src/playtest_pulse/ingestion/__init__.py
@@ -1,8 +1,3 @@
-from playtest_pulse.ingestion.sample_data_generator import generate_sample_events
-
-__all__ = [
-    "generate_sample_events",
-]
 from playtest_pulse.ingestion.csv_loader import load_events_csv
 from playtest_pulse.ingestion.event_validation import validate_raw_event_row
 from playtest_pulse.ingestion.sample_data_generator import generate_sample_events
